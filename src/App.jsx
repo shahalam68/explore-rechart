ab5ffacde5e36c74afd96bb52a3564617b88abf8
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-08-19 22:55:45 +0600

    last

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import MyLineChart from './components/MyLineChart/MyLineChart'
+
+function App() {
+
+  return (
+    <>
+      <MyLineChart></MyLineChart>
+    </>
+  )
+}
+
+export default App
